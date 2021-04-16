92a55f317c7df46f82058621d8b4818bd0671ed5
Michael Rosen <mrosen86@gmail.com>
2021-04-15 19:07:17 -0700

    created sub folders for components

--- public/client/src/components/button/__test__/button.test.jsx
+++ public/client/src/components/button/__test__/button.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '../button';
 
+// eslint-disable-next-line no-undef
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Button />, div);
--- public/client/src/index.jsx
+++ public/client/src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Button from './components/button/button';
 
 const App = () => (
   <div className="App">
     <Button label="click me please" />
+    <div>So Fetch</div>
   </div>
 );
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
 export default App;
