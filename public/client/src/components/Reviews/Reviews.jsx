5437f49fbf0c62d3f68f948b1a73665e6d3d132d
denniswang10 <denniswang2100g@gmail.com>
2021-04-19 10:22:03 -0700

    Added placeholders for individual sections

--- public/client/src/components/Reviews/Reviews.jsx
+++ public/client/src/components/Reviews/Reviews.jsx
@@ -1,1 +1,16 @@
-// Placeholder
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Reviews = () => (
+  <Container fluid>
+    <Row>
+      <Col> Ratings and Reviews </Col>
+    </Row>
+    <Row>
+      <Col xs={6} md={4}> Container for review stars</Col>
+      <Col>Container for comment section</Col>
+    </Row>
+  </Container>
+);
+
+export default Reviews;
