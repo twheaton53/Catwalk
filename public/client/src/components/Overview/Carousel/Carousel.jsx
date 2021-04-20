b3264dc3a2883afb7f540d951146f50c59dfef5f
Michael Rosen <mrosen86@gmail.com>
2021-04-20 16:00:26 -0700

    implemented basic thumbnails

--- public/client/src/components/Overview/Carousel/Carousel.jsx
+++ public/client/src/components/Overview/Carousel/Carousel.jsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
 import styled from 'styled-components';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const StyledSection = styled.section`
   border: 1px solid #4CAF50;
@@ -12,28 +13,43 @@ const StyledSection = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-  img {
+  .slide-active {
     height: 600px;
     aspect-ratio: 3/4;
     object-fit: cover;
     border-radius: 10px;
   }
+  .thumb {
+    height: 60px;
+    width: 60px;
+    object-fit: cover;
+    border-radius: 10px;
+    margin: 3px;
+  }
+
+  .thumbnails {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
   .arrows {
     width: 600px;
   }
   .left-arrow {
     position: absolute;
     font-size: 2rem;
-    right: 150px;
+    left: 30px;
+    top: 50%;
     cursor: pointer;
     z-index: 10;
     user-select: none;
   }
-
   .right-arrow {
     position: absolute;
     font-size: 2rem;
-    left: 150px;
+    right: 30px;
+    top: 50%;
     cursor: pointer;
     z-index: 10;
     user-select: none;
@@ -52,13 +68,24 @@ const Carousel = ({ currentStyle }) => {
   };
   return (
     <StyledSection>
-      {current < photos.length - 1 && <FaArrowCircleRight className="right-arrow" onClick={nextSlide} />}
-      {current > 0 && <FaArrowCircleLeft className="left-arrow" onClick={previousSlide} />}
-      {photos.map((photo, index) => (
-        <div className={index === current ? 'slide-active' : 'slide'} key={index}>
-          {index === current && (<img src={photo.url} alt="" />)}
-        </div>
-      ))}
+      <Container>
+        <Row>
+          <Col className="thumbnails">
+            {photos.map((photo, index) => (
+              <img src={photo.thumbnail_url} alt="" className="thumb" key={index} />
+            ))}
+          </Col>
+          <Col>
+            {current < photos.length - 1 && <FaArrowCircleRight className="right-arrow" onClick={nextSlide} />}
+            {current > 0 && <FaArrowCircleLeft className="left-arrow" onClick={previousSlide} />}
+            {photos.map((photo, index) => (
+              <div>
+                {index === current && (<img src={photo.url} alt="" className={index === current ? 'slide-active' : 'slide'} key={index} />)}
+              </div>
+            ))}
+          </Col>
+        </Row>
+      </Container>
     </StyledSection>
   );
 };
