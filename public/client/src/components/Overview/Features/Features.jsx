f51e4554f7d08624a41e66c3c1f0e8343098c5bb
Michael Rosen <mrosen86@gmail.com>
2021-04-29 19:17:47 -0700

    added features section

---
+++ public/client/src/components/Overview/Features/Features.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const Features = ({ currentProduct }) => {
+  const { features } = currentProduct;
+  console.log(features);
+  return (
+    <div className="features">
+      {features.map((item, index) => (
+        <p style={{ margin: '1px', fontSize: '.85vw' }}>
+          {item.feature}
+          {' '}
+          {item.value && `: ${item.value}`}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Features;
